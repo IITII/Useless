b23b3d6f643721b965819d0129e93ec4fd61b08f
IITII <ccmejx@gmail.com>
2019-08-10 21:54:50 +0800

    update

--- sysInfo_notify.sh
+++ sysInfo_notify.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # simple system notify shell srcipt.
+#git clone https://github.com/GoogleCloudPlatform/slack-samples.git
+#cd slack-samples/notify
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd $script_dir && wget https://raw.githubusercontent.com/IITII/Useless/master/info.sh
+cd $script_dir && wget -O info.sh https://raw.githubusercontent.com/IITII/Useless/master/info.sh
 message=$(bash "${script_dir}/info.sh" -s)
 hook=$(cat "${script_dir}/slack-hook")
 curl -X POST --data-urlencode "payload={\"text\": \"${message}\"}" "${hook}"

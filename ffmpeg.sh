54930a1496257217607688a9a116842a81180487
IITII <ccmejx@gmail.com>
2019-08-27 07:21:52 +0800

    添加了裁剪视频和调整视频速度的功能

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -2,9 +2,9 @@
 #=================================================
 #	Recommend OS: Debian/Ubuntu
 #	Description: A Simple bash script to deal with audio & video
-#   Update：2019年 05月 01日 星期三 01:10:48 CST
+#   Update：2019年 08月 27日 星期三 01:10:48 CST
 #   Create：2019年 04月 30日 星期三 20:10:48 CST
-#	Version: 1.0.0
+#	Version: 1.0.1
 #	Author: IITII
 #	Blog: https://IITII.github.io
 #=================================================
@@ -45,6 +45,37 @@ srt_to_ass() {
 add_ass() {
     ffmpeg -i $1 -vf "ass=$2" $3
 }
+#剪切视频
+# @param $1 输入视频的名称
+# @param $2 截取开始时间
+# @param $3 截取长度
+# @param $4 输出视频名称
+#用 -ss 和 -t 选项， 从第 30 秒开始，向后截取 10 秒的视频，并保存：
+cut_video_t() {
+    ffmpeg -i $1 -ss $2 -c copy -t $3 $4
+}
+#达成相同效果，也可以用 -ss 和 -to 选项， 从第 30 秒截取到第 40 秒：
+# @param $1 输入视频的名称
+# @param $2 截取开始时间
+# @param $3 截取结束时间
+# @param $4 输出视频名称
+cut_video_to() {
+    ffmpeg -i $1 -ss $2 -c copy -to $3 $4
+}
+# 值得注意的是，ffmpeg 为了加速，会使用关键帧技术， 所以有时剪切出来的结果在起止时间上未必准确。
+# 通常来说，把 -ss 选项放在 -i 之前，会使用关键帧技术；
+# 把 -ss 选项放在 -i 之后，则不使用关键帧技术。
+# 如果要使用关键帧技术又要保留时间戳，可以加上 -copyts 选项：
+# ffmpeg -ss 00:01:00 -i video.mp4 -to 00:02:00 -c copy -copyts cut.mp4
+
+## @param $1 输入视频的名称
+# @param $2 倍速
+# @param $3 输出视频名称
+speed_adjust() {
+    # 加速四倍：setpts=0.25*PTS
+    # 四倍慢速：setpts=4*PTS
+    ffmpeg -i $1 -vf "setpts=$2*PTS" $3
+}
 #交互选择界面
 cmd_interaction() {
     echo -e "请选择你要选择的操作的序号：\n"
@@ -58,6 +89,9 @@ cmd_interaction() {
     echo -e "7.添加srt字幕"
     echo -e "8.srt字幕转ass"
     echo -e "9.添加ass字幕"
+    echo -e "10.按开始时间和截取长度截取视频"
+    echo -e "11.按开始时间和结束时间截取视频"
+    echo -e "12.更改视频的速度"
     read i
     case $i in
     1)
@@ -110,6 +144,29 @@ cmd_interaction() {
         read -p "请输入要获取的视频文件名称: " output_mp4
         add_ass $input_mp4 $input_ass $output_mp4
         ;;
+    10)
+        read -p "请输入音频文件名称：" input_mp4
+        read -p "截取开始时间：" start
+        read -p "截取长度：" last
+        echo -e "时间格式：00:00:00 (时：分：秒)"
+        read -p "请输入要获取的文件名称: " output_mp4
+        cut_video_t $input_mp4 $start $last $output_mp4
+        ;;
+    11)
+        read -p "请输入音频文件名称：" input_mp4
+        read -p "截取开始时间：" start
+        read -p "截取结束时间：" end
+        echo -e "时间格式：00:00:00 (时：分：秒)"
+        read -p "请输入要获取的文件名称: " output_mp4
+        cut_video_to $input_mp4 $start $end $output_mp4
+        ;;
+    12)
+        read -p "请输入视频文件名称：" input_mp4
+        read -p "要调整的速度（输入0.25是4倍，输入4是0.25倍）：" speed
+        read -p "请输入要获取的视频文件名称: " output_mp4
+        speed_adjust $input_mp4 $speed $output_mp4
+        ;;
+
     *)
         echo "请输入有效数字！！！"
         ;;
@@ -161,6 +218,16 @@ while getopts "m:he" arg; do #选项后面的冒号表示该选项需要参数
         9)
             add_ass $1 $2 $3
             ;;
+        10)
+            cut_video_t $1 $2 $3 $4
+            ;;
+        11)
+            cut_video_to $1 $2 $3 $4
+            ;;
+        12)
+            speed_adjust $1 $2 $3
+            ;;
+
         *)
             echo "请输入有效数字！！"
             ;;
@@ -174,11 +241,14 @@ while getopts "m:he" arg; do #选项后面的冒号表示该选项需要参数
         echo "              -m 2: 获取mp3文件"
         echo "              -m 3: 获取H.264视频流文件(.mp4文件)"
         echo "              -m 4: 整合mp4文件和mp4文件"
-        echo "              -m 5: .整合mp4和mp3文件"
+        echo "              -m 5: 整合mp4和mp3文件"
         echo "              -m 6: 整合mp4和aac文件"
         echo "              -m 7: 添加srt字幕"
         echo "              -m 8: srt字幕转ass"
         echo "              -m 9: 添加ass字幕"
+        echo "              -m 10: 按开始时间和截取长度截取视频"
+        echo "              -m 11: 按开始时间和结束时间截取视频"
+        echo "              -m 12: 更改视频的速度"
         ;;
     e)
         echo -e "./ffmpeg.sh -m 1 input.mp4 output.aac"
@@ -190,6 +260,12 @@ while getopts "m:he" arg; do #选项后面的冒号表示该选项需要参数
         echo -e "./ffmpeg.sh -m 7 input.mp4 input.srt output.mp4"
         echo -e "./ffmpeg.sh -m 8 input.srt output.ass"
         echo -e "./ffmpeg.sh -m 9 input.mp4 input.ass output.mp4"
+        echo -e "截取长度为30秒的视频："
+        echo -e "./ffmpeg.sh -m 10 input.mp4 00:00:10 00:00:30 output.mp4"
+        echo -e "截取长度为30秒的视频："
+        echo -e "./ffmpeg.sh -m 11 input.mp4 00:00:10 00:00:40 output.mp4"
+        echo -e "将视频的播放速度调整为4倍："
+        echo -e "./ffmpeg.sh -m 12 input.mp4 0.25 output.mp4"
         ;;
     ?) #当有不认识的选项的时候arg为?
         echo "unkonw argument"

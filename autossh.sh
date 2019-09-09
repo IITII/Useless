8a0c5e73814cf4f1d9cf7bf982a4a491966158ff
IITII <ccmejx@gmail.com>
2019-09-09 14:46:21 +0800

    A simple ssh script

---
+++ autossh.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#=================================================
+#	Recommend OS: Debian/Ubuntu
+#	Description: CUDA version of ffmpeg.sh
+#   Update：2019年 08月 27日 08:10:48 CST
+#   Create：2019年 08月 27日 08:10:48 CST
+#	Version: 1.0.0
+#	Author: IITII
+#	Blog: https://IITII.github.io
+#=================================================
+# curl https://raw.githubusercontent.com/IITII/Useless/master/autossh.sh | bash
+mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bk
+mv /root/authorized_keys /root/authorized_keys.bk
+wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/IITII/Useless/master/ssh/sshd_config
+wget -O /root/authorized_keys https://raw.githubusercontent.com/IITII/Useless/master/ssh/authorized_keys
+service ssh restart && service sshd restart

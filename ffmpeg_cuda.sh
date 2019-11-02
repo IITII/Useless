df1f036f1dbb455c844cf10cc0086cd367f1477b
IITII <ccmejx@gmail.com>
2019-11-02 15:52:42 +0800

    ws+tls+web

---
+++ ffmpeg_cuda.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#=================================================
+#	Recommend OS: Debian/Ubuntu
+#	Description: CUDA version of ffmpeg.sh
+#   ffmpeg: (https://raw.githubusercontent.com/IITII/Useless/master/ffmpeg.sh)
+#   Update：2019年 08月 27日 08:10:48 CST
+#   Create：2019年 08月 27日 08:10:48 CST
+#	Version: 1.0.0
+#	Author: IITII
+#	Blog: https://IITII.github.io
+#=================================================
+encode_as_h264(){
+    
+}
+ 
